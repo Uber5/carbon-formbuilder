df754a8a70b227aba8180e722c21ec7e24241c15
Chris <chris@uber5.com>
2019-07-24 17:37:32 +0200

    select-multi is functional

--- src/components/SelectMulti/SelectMulti.js
+++ src/components/SelectMulti/SelectMulti.js
@@ -1,5 +1,29 @@
 import React from 'react'
-
+import useOptions from  '../../lib/useOptions'
+import { SelectSkeleton, MultiSelect } from  'carbon-components-react'
 export default ({ formikProps, formProps, ...field }) => {
-  return <p>multi...</p>
+  const options = useOptions(field)
+
+  const { touched, errors, setFieldValue } = formikProps
+
+  if (!options) {
+    return <SelectSkeleton />
+  }
+
+  const Component = field.searchable ? MultiSelect.Filterable : MultiSelect
+
+  return <Component
+    {...field}
+    name={field.name}
+    label={field.label}
+    titleText={field.label}
+    invalid={touched[name] && errors[name] !== undefined}
+    invalidText={touched[name] && errors[name]}
+    items={options}
+    itemToString={option => option.name}
+    onChange={({ selectedItems }) => {
+      console.log('MultiSelect onChange, e', selectedItems)
+      setFieldValue(field.name, selectedItems.map(e => e.value))
+    }}
+  />
 }
